c95d8f90f53b44bd8bd3734883e333f964d92620
ardee <ardee@ArdeeJarelle>
2022-09-27 20:24:56 -0600

    Implemented Serializable in Note.java

--- src/java/models/Note.java
+++ src/java/models/Note.java
@@ -1,14 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package models;
+
+import java.io.Serializable;
 
 /**
  *
  * @author ardee
  */
-public class Note {
+public class Note implements Serializable {
+    private String title;
+    private String contents;
     
+    public Note() {
+        title = "";
+        contents = "";
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
 }
+
